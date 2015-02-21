7beefac07e0be13a64744d52d7d8d49d90fe0e8b
Ian McDevitt <ian.mcdevitt.4@gmail.com>
2015-02-21 14:51:22 -0500

    Point angular templates to the appropriate directory

--- public/dnd/js/app.js
+++ public/dnd/js/app.js
@@ -10,12 +10,12 @@ angular.module('myApp', [
     'dnd5e.filters'
 ])
 .config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
-    $routeProvider.when('/spells/', {templateUrl: 'partials/spells.html', controller: 'spellsCtrl'});
-    $routeProvider.when('/spells/:levels', {templateUrl: 'partials/spells.html', controller: 'spellsCtrl'});
-    $routeProvider.when('/spells/:levels/:classes', {templateUrl: 'partials/spells.html', controller: 'spellsCtrl'});
-    $routeProvider.when('/spells/:levels/:classes/:schools', {templateUrl: 'partials/spells.html', controller: 'spellsCtrl'});
-    $routeProvider.when('/spells/:levels/:classes/:schools/:selected', {templateUrl: 'partials/spells.html', controller: 'spellsCtrl'});
-    $routeProvider.when('/shapeshift/', {templateUrl: 'partials/shapeshift.html', controller: 'shapeshiftCtrl'});
+    $routeProvider.when('/spells/', {templateUrl: 'public/partials/spells.html', controller: 'spellsCtrl'});
+    $routeProvider.when('/spells/:levels', {templateUrl: 'public/partials/spells.html', controller: 'spellsCtrl'});
+    $routeProvider.when('/spells/:levels/:classes', {templateUrl: 'public/partials/spells.html', controller: 'spellsCtrl'});
+    $routeProvider.when('/spells/:levels/:classes/:schools', {templateUrl: 'public/partials/spells.html', controller: 'spellsCtrl'});
+    $routeProvider.when('/spells/:levels/:classes/:schools/:selected', {templateUrl: 'public/partials/spells.html', controller: 'spellsCtrl'});
+    $routeProvider.when('/shapeshift/', {templateUrl: 'public/partials/shapeshift.html', controller: 'shapeshiftCtrl'});
     $routeProvider.otherwise('/404');
     $httpProvider.defaults.useXDomain = true;
 }]);
