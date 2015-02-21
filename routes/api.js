596519ee32b57202ea7a369932e81ab9a967a759
Ian McDevitt <ian.mcdevitt.4@gmail.com>
2015-02-21 11:25:50 -0500

    Fix database configuration

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@
  * Serve JSON to our AngularJS client
  */
 var dbconfig = require('../database.json');
-var knex = require('knex')(dbconfig[process.env.NODE_ENV || 'development']);
+var knex = require('knex')(dbconfig[process.env.NODE_ENV || 'development'].knex);
 
 
 exports.name = function (req, res) {
