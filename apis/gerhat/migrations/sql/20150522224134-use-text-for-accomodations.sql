77914d2b26a128e1041692cd491b98502db157cf
Ian McDevitt <ian@mcdevi.tt>
2015-05-22 19:29:45 -0400

    Fix migration

--- apis/gerhat/migrations/sql/20150522224134-use-text-for-accomodations.sql
+++ apis/gerhat/migrations/sql/20150522224134-use-text-for-accomodations.sql
@@ -1,4 +1,4 @@
 ALTER TABLE invitations MODIFY accomodations TEXT;
-ALTER TABLE invitations ADD friday varchar(255) DEFAULT NULL;
-ALTER TABLE invitations ADD saturday varchar(255) DEFAULT NULL;
-ALTER TABLE invitations ADD sunday varchar(255) DEFAULT NULL;
+ALTER TABLE invitations ADD friday BOOLEAN DEFAULT NULL;
+ALTER TABLE invitations ADD saturday BOOLEAN DEFAULT NULL;
+ALTER TABLE invitations ADD sunday BOOLEAN DEFAULT NULL;
