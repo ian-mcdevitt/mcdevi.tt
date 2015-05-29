9b6ed9f47e5c923d727d478cc2f234a66d4f1751
Ian McDevitt <ian@mcdevi.tt>
2015-05-29 15:44:43 -0400

    Add gallery link

--- apis/chelsea/routes.js
+++ apis/chelsea/routes.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.all('', function(req, res) {
-    res.redirect(301, 'http://www.chelsealynphotography.com');
+router.all('*', function(req, res) {
+    res.redirect(301, 'http://www.chelsealynphotography.com' + req.url);
 });
 
 module.exports = router;
