5049baba8bc1c8b92c0c108b3ca9a4f2c74b1a90
Ian McDevitt <ian@mcdevi.tt>
2015-06-04 19:51:58 -0400

    Finish making shit pretty

--- public/gerhat/js/controllers/navCtrl.js
+++ public/gerhat/js/controllers/navCtrl.js
@@ -3,7 +3,6 @@
 angular.module('wedding.controllers.nav', []).controller('navCtrl', ['$scope', '$location',
     function($scope, $location) {
         $scope.isActive = function(route) {
-            console.log($location.path())
             return route === $location.path();
         }
     }
