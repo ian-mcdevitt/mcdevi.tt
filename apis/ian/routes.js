eb4da2ef9c50b034b8ca5f7f7585bedf7d293563
Ian McDevitt <ian@mcdevi.tt>
2015-06-22 20:57:15 -0400

    Replace startup css

--- apis/ian/routes.js
+++ apis/ian/routes.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('*', function(req, res, next) {
+router.get('/apiroute', function(req, res, next) {
     res.send(200);
 });
 
