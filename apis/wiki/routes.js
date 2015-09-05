8bca79e7f02ec5c1868da12710eebaf6fa16b9dd
Ian McDevitt <ian@mcdevi.tt>
2015-09-05 15:14:44 -0400

    [WIKI] Fix content-types

--- apis/wiki/routes.js
+++ apis/wiki/routes.js
@@ -6,9 +6,11 @@ router.get('/*', function(req, res, next) {
     // Pick either get or post, but not GET or POST
     var method = req.method.toLowerCase()
     http[method]({ url: 'http://mcdevi.tt:4567' + req.url }, function(error, result) {
-        // Set it to text/html, because browsers will know that an image with that content type is an image
-        // They won't know that HTML with `application/octet-stream` is HTML
-        res.set('Content-Type', 'text/html');
+        if(req.url.indexOf('.css') !== -1) {
+            res.set('Content-Type', 'text/css');
+        } else {
+            res.set('Content-Type', 'text/html');
+        }
         res.send(result.buffer);
     });
 
