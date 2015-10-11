4b664b7e22720bd7bfb5abb2b84fd616b0695abe
Ian McDevitt <ian@mcdevi.tt>
2015-10-11 10:47:50 -0400

    [GERHAT] Post-wedding update

--- public/gerhat/js/controllers/homeCtrl.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-/* Controllers */
-angular.module('wedding.controllers.home', []).controller('homeCtrl', [
-    function() {
-        $('#countdown').countdown('2015/09/20 11:00:00', function(event) {
-            $(this).html(event.strftime('%D days, %H hours, %M minutes, %S seconds'));
-        });
-    }
-]);
