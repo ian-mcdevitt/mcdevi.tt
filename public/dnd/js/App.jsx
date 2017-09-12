2d34ceac39a1b364681ef6bf13486f14685b9908
Ian McDevitt <ian@clevertech.biz>
2017-09-12 12:26:35 -0400

    React rebuild v0.0.1

---
+++ public/dnd/js/App.jsx
@@ -0,0 +1,23 @@
+// @flow
+
+import React from 'react';
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// import type { Match } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+
+const FourOhFour = () => <h1>404</h1>;
+
+const App = () => <FourOhFour />;
+/*
+    <BrowserRouter>
+        <Provider>
+            <div className="app">
+                <Switch>
+                    <Route component={FourOhFour} />
+                </Switch>
+            </div>
+        </Provider>
+    </BrowserRouter>;
+*/
+
+export default App;
